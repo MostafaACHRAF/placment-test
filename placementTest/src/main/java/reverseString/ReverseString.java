0821e6301f99b7915d0c39694e4fa4a834631947
Mostafa <masharf@sqli.com>
2018-02-07 10:47:29 +0000

    reverse string

--- placementTest/src/main/java/reverseString/ReverseString.java
+++ placementTest/src/main/java/reverseString/ReverseString.java
@@ -2,13 +2,20 @@ package reverseString;
 
 public class ReverseString {
 
-    private String reverseString(String string) {
-        String[] reversedStrin
+    private static String reverseString(String string) {
+        char[] reversedchars = new char[string.length()];
+        char[] s = string.toCharArray();
+        int index =0;
+        for (int i = string.length() - 1; i > 0; i--) {
+            reversedchars[index] = s[i];
+            index++;
+        }
+        System.out.println(reversedchars);
+        return String.valueOf(reversedchars);
     }
 
     public static void main(String[] args) {
         String string = "abcdef";
-
-
+        reverseString(string);
     }
 }
